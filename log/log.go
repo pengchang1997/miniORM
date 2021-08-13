f1c81a22bff0c560a1b5dfe27ee1d12d5b44251c
changchang <pengchang1997@126.com>
2021-08-13 18:44:26 +0800

    日志库实现

--- log/log.go
+++ log/log.go
@@ -12,7 +12,7 @@ var (
 	errorLog = log.New(os.Stdout, "\033[31m[error]\033[0m ", log.LstdFlags|log.Lshortfile)
 
 	// info为蓝色
-	infoLog = log.New(os.Stdout, "\033[34m[error]\033[0m ", log.LstdFlags|log.Lshortfile)
+	infoLog = log.New(os.Stdout, "\033[34m[info]\033[0m ", log.LstdFlags|log.Lshortfile)
 
 	// Logger类型表示一个活动状态的记录日志的对象，它会生成一行行的输出写入一个io.Writer接口
 	// 每一条日志操作会调用一次io.Writer接口的Write方法
