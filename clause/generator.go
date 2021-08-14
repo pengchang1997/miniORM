606d139032c1c42b9385edafba35d3c13a1b2df0
changchang <pengchang1997@126.com>
2021-08-14 21:48:20 +0800

    实现子句生成器

---
+++ clause/generator.go
@@ -0,0 +1,81 @@
+package clause
+
+import (
+	"fmt"
+	"strings"
+)
+
+// 实现各个子句的生成规则
+
+type generator func(values ...interface{}) (string, []interface{})
+
+var generators map[Type]generator
+
+func init() {
+	generators = make(map[Type]generator)
+	generators[INSERT] = _insert
+	generators[VALUES] = _values
+	generators[SELECT] = _select
+	generators[LIMIT] = _limit
+	generators[WHERE] = _where
+	generators[ORDERBY] = _orderBy
+}
+
+func genBindVars(num int) string {
+	var vars []string
+	for i := 0; i < num; i++ {
+		vars = append(vars, "?")
+	}
+
+	return strings.Join(vars, ", ")
+}
+
+// 生成insert子句
+func _insert(values ...interface{}) (string, []interface{}) {
+	tableName := values[0]
+	fields := strings.Join(values[1].([]string), ",")
+	return fmt.Sprintf("INSERT INTO %s (%v)", tableName, fields), []interface{}{}
+}
+
+// 生成values子句
+func _values(values ...interface{}) (string, []interface{}) {
+	var bindStr string
+	var sql strings.Builder
+	var vars []interface{}
+	sql.WriteString("VALUES ")
+	for i, value := range values {
+		v := value.([]interface{})
+		if bindStr == "" {
+			bindStr = genBindVars(len(v))
+		}
+		sql.WriteString(fmt.Sprintf("(%v)", bindStr))
+		if i+1 != len(values) {
+			sql.WriteString(", ")
+		}
+		vars = append(vars, v...)
+	}
+	return sql.String(), vars
+}
+
+// 生成select子句
+func _select(values ...interface{}) (string, []interface{}) {
+	tableName := values[0]
+	fields := strings.Join(values[1].([]string), ",")
+	return fmt.Sprintf("SELECT %v FROM %s", fields, tableName), []interface{}{}
+}
+
+// 生成limit子句
+func _limit(values ...interface{}) (string, []interface{}) {
+	return "LIMIT ?", values
+}
+
+// 生成where子句
+func _where(values ...interface{}) (string, []interface{}) {
+	desc, vars := values[0], values[1:]
+	return fmt.Sprintf("WHERE %s", desc), vars
+}
+
+// 生成order by子句
+func _orderBy(values ...interface{}) (string, []interface{}) {
+	return fmt.Sprintf("ORDER BY %s", values[0]), []interface{}{}
+}
